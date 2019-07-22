299853e2bc77f4a24f6d09c8b03b28dd6e88b898
KarinMishor <34835251+KarinMishor@users.noreply.github.com>
2019-07-22 10:51:32 +0300

    Update DEV_NID_VER.py
    add comments

--- DEV_NID_VER.py
+++ DEV_NID_VER.py
@@ -1,6 +1,25 @@
 import time
 from NID import NID
 
+
+# The output (path taken from the UI): (1)Two csv files ,(2) Heat-Map for pairwise interactions, (3)Log file
+    # 1st csv file for pairwise interaction
+    # 2nd csv file for higher order interaction
+    # every line contains interaction and strength
+          
+  '''input params, init format:
+    use_main_effect_nets - whether use main effects or not (true / false)
+    use_cutoff - whether use cutoff or not (true / false)
+    is_index_col - is index column exists (1 true / 0 false)
+    is_header - is header exists (1 true / 0 false)
+    file_path - full path
+    out_path - full path
+    units_list - network architecture (list of numbers seperate by comma)
+    is_classification_col - is classification dataset (1 true / 0 false, false means regression)
+    k_fold_entry - number of folds (int, greater than 2)
+    num_epochs_entry -  number of epochs (int, greater than 1)
+    batch_size_entry -  number of batches (int, greater than 1)
+    '''
 file_path = r'C:\Users\Ilana\PycharmProjects\Neural_Interaction_Detection\datasets\higgs\higgs.csv'
 output_path = r'C:\Users\Ilana\PycharmProjects\Neural_Interaction_Detection\datasets\higgs'
 is_classification = 1
